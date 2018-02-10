730fb8989adc8fce7733d589a4b4640c9601a149
Matthew Anderson <matt10.anderson@gmail.com>
2018-02-10 09:39:46 -0500

    Add project files.

---
+++ program4.asm
@@ -0,0 +1,25 @@
+TITLE Program 4     (program4.asm)
+
+; Author: Matthew Anderson		   anderma8@oregonstate.edu
+; CS 271 Program 4                 Date: February 10, 2018
+; Description:
+
+INCLUDE Irvine32.inc
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	exit	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
