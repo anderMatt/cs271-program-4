2d827f55817e43130d7876ef9d2f1043a64549dd
Matthew Anderson <matt10.anderson@gmail.com>
2018-02-10 14:52:36 -0500

    Successfully computes and prints first N composite numbers.

--- program4.asm
+++ program4.asm
@@ -19,6 +19,18 @@ myName			BYTE	"Written by: Matthew Anderson",0
 inputPrompt		BYTE	"Enter a value for N in [1, 400], and I will display the first N composite numbers: ",0
 errInputPrompt	BYTE	"Number must be in [1, 400]. Try again: ",0
 
+currNum			DWORD	3
+printTotal		DWORD	?		;number of composite numbers user wants us to print
+printCount		DWORD	0		;number of composite numbers we have already printed.
+
+gutter			BYTE	"   ",0	
+
+
+;---------------------------
+; FOR TESTING
+;---------------------------
+yesStr BYTE "that IS composite", 0
+noStr BYTE	"that is NOT composite", 0
 
 
 .code
@@ -27,6 +39,10 @@ main PROC
 ; (insert executable instructions here)
 	call	Introduction
 	call	GetUserData
+	call	Initialize
+	call	PrintComposites
+	; TODO: call Farewell
+
 
 	exit	; exit to operating system
 main ENDP
@@ -60,7 +76,7 @@ GetUserData PROC
 	mov		edx, OFFSET inputPrompt
 	call	WriteString
 
-readInput:  call ReadInt	;Read integer entered by user
+readInput:  call ReadInt		;Read integer entered by user
 	call	Validate
 	jz		inputValid
 
@@ -69,9 +85,25 @@ readInput:  call ReadInt	;Read integer entered by user
 	call	WriteString
 	jmp		readInput
 
-inputValid:	ret
+inputValid:	
+	;mov		printTotal, eax		;Save number of composites user wants us to print
+	ret
 GetUserData ENDP
 
+
+;--------------------------------------------------
+Initialize PROC
+
+; Initializes registers to computing the first N
+; composite numbers.
+; Pre-conditions: EAX is number of composite numbers
+; to print.
+;--------------------------------------------------
+	mov		ecx, eax
+	;dec		ecx
+	ret
+
+Initialize ENDP
 
 
 ;--------------------------------------------------
@@ -84,12 +116,122 @@ Validate PROC
 ;--------------------------------------------------
 
 	cmp		eax, LOWER_LIMIT
-	jb		notValid			;ZF=0 if dest < source
+	jb		notValid			;#2ZF=0 if dest < source
 	cmp		eax, UPPER_LIMIT
-	ja		notValid			;ZF=0 if dest > source
-	test	eax, 0				;ZF = 1
+	ja		notValid			;#2ZF=0 if dest > source
+	test	eax, 0				;#5ZF = 1
 
-notValid:	ret					;ZF = 0
+notValid: ret					;#?ZF = 0
 Validate ENDP
+
+
+;--------------------------------------------------
+IsComposite PROC
+
+; Checks if the current number in EAX is composite.
+; Receives: EAX = number to check
+; Returns: ZF=1 if number in EAX is composite;
+; otherwise, ZF=0
+;
+; USES ebx, edx, esi
+;--------------------------------------------------
+
+	;Check if currNumber is divisible by any number in [2, currNumber], other than itself.
+	mov		ebx, 2
+
+;check if currNumber is divisible by next number in [2, currNumber] sequence
+next:	
+	mov		esi, eax
+	cmp		eax, ebx
+	je		no			;Not divisible by anything in [2, currNum], so not composite.
+
+	xor		edx, edx
+	div		ebx
+	cmp		edx, 0		;is divisible
+	je		done	
+
+	inc		ebx			;set EBX to next divisor in [2, currNum]
+	mov		eax, esi
+	jmp		next
+
+no:
+	cmp edx, 0			;set ZF = 0. We know EDX is never 0 here, otherwise, it would be composite.
+
+done: ret
+
+IsComposite ENDP
+
+
+;--------------------------------------------------
+GetNextComposite PROC
+; Checks if currNum is a composite number.
+; Returns:  currNum = the next composite number.
+;--------------------------------------------------
+
+checkNext:
+	inc		currNum
+	mov		eax, currNum
+	call	IsComposite
+	jnz		checkNext
+	ret
+
+GetNextComposite ENDP
+
+
+
+;--------------------------------------------------
+PrintComposites PROC
+; Prints the first N composite numbers.
+
+; Pre-conditions: ECX=N, the number of composite
+; numbers to print.
+;--------------------------------------------------
+
+doNext:
+	call	GetNextComposite
+	call	Print
+	loop	doNext
+	ret
+
+PrintComposites ENDP
+
+
+;--------------------------------------------------
+Print PROC
+; Prints currNum
+;--------------------------------------------------
+	mov		edx, OFFSET gutter
+	call	WriteString
+
+	push	eax		;Preserve registers.
+	push	ebx
+	push	edx
+
+	;Print current composite number.
+	mov		eax, currNum
+	call	WriteDec
+	inc		printCount
+
+	;Check if we need a new line - print count divisible by 10.
+	mov		eax, printCount
+	xor		edx, edx
+	mov		ebx, 10
+	div		ebx
+
+	cmp		edx, 0
+	jne		done
+
+	;print a new line
+	call	CrLf
+
+done:
+	pop		edx
+	pop		ebx
+	pop		eax
+	ret
+
+Print ENDP
+
+
 
 END main
